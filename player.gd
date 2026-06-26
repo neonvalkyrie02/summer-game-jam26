91dcd32f3a8316b14c8f0ebc3a969b1119ea9cc6
neonvalkyrie02 <118484681+neonvalkyrie02@users.noreply.github.com>
2026-06-26 20:51:51 +0200

    animation tree done

--- player.gd
+++ player.gd
@@ -1,24 +1,38 @@
 extends CharacterBody2D
 @onready var _animation_player = $AnimationPlayer
+@onready var _animation_tree = $AnimationTree
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var sit = false
+var use = false
 
+func _ready(): 
+	_animation_tree.set_active(true) 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	
+	use = false
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		
+	if Input.is_action_just_pressed("ui_down") and is_on_floor():
+		sit = true
+		
+	if Input.is_action_just_pressed("action_use") and is_on_floor():
+		use = true
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		sit = false
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
